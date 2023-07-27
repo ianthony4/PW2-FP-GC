a086a508c9a3d1cf7d6c7b3ac7825d76e651f15d
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-27 08:29:10 -0500

    agregado los modelos de catalogo de cuentas, añadido para descargar en pdf(no terminado),

--- gestion_de_clientes/apps/catalogo_cuentas/models.py
+++ gestion_de_clientes/apps/catalogo_cuentas/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class  Cuenta(models.Model):
+    type_account = models.CharField(max_length=100)
+    activos = models.IntegerField()
+    pasivos = models.IntegerField()
+    id_cuenta = models.IntegerField()
+    mov_deudor = models.IntegerField()
+    mov_acreedor = models.IntegerField()
+
+    def __str__(self):
+        return self.id_cuenta
+class Banco(models.Model):
+    name = models.CharField(max_length=100)
+    type_bank = models.CharField(max_length=100)
+    description = models.TextField()
+
+class CatalogoCuentas(models.Model):
+    name = models.CharField(max_length=100)
+    id_created = models.CharField(max_length=100)
+    type_catalog = models.CharField(max_length=100)
+    accounts = models.ManyToManyField(Cuenta, )
+    bancos = models.ManyToManyField(Banco)
+    # clientes = models.ManyToManyField(Clientes)
+    def __str__(self):
+        return self.name
--- gestion_de_clientes/apps/catalogo_cuentas/views.py
+++ gestion_de_clientes/apps/catalogo_cuentas/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .forms import RawCatalogoForm,RawCuentaForm
+from django.views.generic import View
+from django.template.loader import get_template
+from .utils import render_to_pdf #created in step 4
 
 # Create your views here.
+
+
+def RawFormCuenta(request):
+    form = RawCuentaForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'create.html',context) 
+def getResponse(request):
+    response = request.POST["type_account"]
+    print(response)
+    return HttpResponse("Hello Wold") 
+
+
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('pdf/invoice.html')
+        context = {
+            'invoice_id': 123, 
+            'customer_name': 'Vladimir Sulla',
+            'amount': 12000,
+            'today': "Today",
+        }
+        pdf = render_to_pdf('pdf/invoice.html',context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %(12341231)
+            content = "inline; filename=%s" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename=%s" %(filename) 
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse('No Found')
